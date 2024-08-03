74f277f414292b06cf66d8a0f8ef1fcb961fa756
mnava1418 <martin@mnavapena.com>
2024-08-03 19:44:47 -0400

    ADD: create sections pages

---
+++ src/views/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <section id='about' style={{height: '300px', backgroundColor: 'blue'}}>About Page</section>
+  )
+}
+
+export default AboutPage
---
+++ src/views/pages/ServicesPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServicesPage = () => {
+  return (
+    <section id='services' style={{height: '500px', backgroundColor: 'red'}}>Services Page</section>
+  )
+}
+
+export default ServicesPage
