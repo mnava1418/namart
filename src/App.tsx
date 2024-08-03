adee125be7274c010e63aeca0ae5d0912756ba7c
mnava1418 <martin@mnavapena.com>
2024-08-03 19:54:42 -0400

    UPDATE: show main page and adjust styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Menu from "./views/components/Menu";
+import MainPage from "./views/pages/MainPage";
+
+import './assets/styles/main.css'
 
 function App() {
   return (
     <>
       <Menu />
+      <MainPage />
     </>
   );
 }
