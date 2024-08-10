746ba4748325ef89b456d9743b7baad41e2365c6
mnava1418 <martin@mnavapena.com>
2024-08-10 16:48:30 -0400

    UPDATE: set menu content

--- src/views/components/Menu.tsx
+++ src/views/components/Menu.tsx
@@ -1,8 +1,33 @@
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { useEffect, useState } from 'react'
 
 import '../../styles/menu.css'
+import { language, MenuContent } from '../../interfaces/content.interface'
 
-const Menu = () => {
+type MenuProps = {
+  content: MenuContent,
+  language: language,
+  setLanguage: React.Dispatch<React.SetStateAction<language>>
+}
+
+const Menu = ({content, language, setLanguage}: MenuProps) => {
+  const {about, english, home, projects, services, spanish} = content
+  const [flag, setFlag] = useState(<><>🇲🇽&nbsp;&nbsp;Es</></>)
+
+  const switchLanguage = (currentLanguage: language) => {
+    setLanguage(currentLanguage)
+
+    if(currentLanguage === 'es') {
+      setFlag(<>🇲🇽&nbsp;&nbsp;Es</>)
+    } else {
+      setFlag(<>🇺🇸&nbsp;&nbsp;En</>)
+    }
+  }
+
+  useEffect(() => {
+    switchLanguage(language)
+  }, [])
+  
   return (
     <Navbar bg="dark" sticky='top' data-bs-theme="dark" expand="lg" className="custom-navbar">
       <Container>
@@ -12,15 +37,19 @@ const Menu = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-center" style={{width: '100%'}}>
-            <Nav.Link href="#home" className='custom-nablink'>Inicio</Nav.Link>
-            <Nav.Link href="#services" className='custom-nablink'>Servicios</Nav.Link>
-            <Nav.Link href="#about" className='custom-nablink'>Sobre mí</Nav.Link>
-            <Nav.Link href="#project" className='custom-nablink'>Proyectos</Nav.Link>
+            <Nav.Link href="#home" className='custom-nablink'>{home}</Nav.Link>
+            <Nav.Link href="#services" className='custom-nablink'>{services}</Nav.Link>
+            <Nav.Link href="#about" className='custom-nablink'>{about}</Nav.Link>
+            <Nav.Link href="#project" className='custom-nablink'>{projects}</Nav.Link>
           </Nav>
           <Nav className="me-auto justify-content-end">
-            <div className='d-flex flex-row justify-content-start'>
-                <Nav.Link href="https://www.instagram.com/namartdev" style={{marginRight: '16px'}} target='_blank'><i className="bi bi-instagram fs-5" /></Nav.Link>
-                <Nav.Link href="https://github.com/mnava1418" target='_blank'><i className="bi bi-github fs-5" /></Nav.Link>
+            <div className='d-flex flex-row justify-content-start align-items-center'>
+                <Nav.Link href="https://www.instagram.com/namartdev" style={{marginRight: '16px'}} target='_blank'><i className="bi bi-instagram fs-6" /></Nav.Link>
+                <Nav.Link href="https://github.com/mnava1418" style={{marginRight: '16px'}} target='_blank'><i className="bi bi-github fs-6" /></Nav.Link>
+                <NavDropdown title={flag} id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#" onClick={() => {switchLanguage('es')}}>🇲🇽&nbsp;&nbsp;{spanish}</NavDropdown.Item>
+                  <NavDropdown.Item href="#" onClick={() => {switchLanguage('en')}}>🇺🇸&nbsp;&nbsp;{english}</NavDropdown.Item>
+              </NavDropdown>
             </div>
           </Nav>
         </Navbar.Collapse>
