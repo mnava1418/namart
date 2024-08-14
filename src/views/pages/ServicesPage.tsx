83f200bba56733f3ff7d1378dbda787347e0b8e9
mnava1418 <martin@mnavapena.com>
2024-08-13 20:02:22 -0400

    UPDATE: service page styles

--- src/views/pages/ServicesPage.tsx
+++ src/views/pages/ServicesPage.tsx
@@ -12,7 +12,7 @@ const ServicesPage = ({content}: ServicesPageProps) => {
     <section id='services' style={{background: 'var(--main-gradient)'}}>
       <div className='custom-container'>
         <h3 style={{margin: '32px 0px 32px 0px', color: '#fff'}}>{title}</h3>
-        <div className='d-flex flex-wrap flex-row justify-content-center align-items-center' style={{marginBottom: '32px'}}>
+        <div className='d-flex flex-wrap flex-row justify-content-center align-items-start' style={{marginBottom: '32px'}}>
           {services.map((card, index) => (
             <CustomCard key={index} icon={card.icon} title={card.title} text={card.text}/>
           ))}
